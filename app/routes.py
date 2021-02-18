fe3827b0dec8b5942d9b69088c15ce333ef3514b
jhon-swai <jhonswai@gmail.com>
2021-02-18 15:29:22 +0300

    api

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template
+from app.models import Score
+
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
@@ -7,9 +10,19 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/leaderboard')
+@app.route('/leaderboard', methods=["POST"])
 def leaderboard():
-    
-    return ""
+    get_scores = Score.query.all()
+    data =[]
+    for score in get_scores:
+        data.append(score.total)
+
+    data_dict = {}
+    num = 0
+    for val in data:
+        data_dict[num]=val
+        num+=1
+
+    return jsonify(data_dict)
     
 
---
+++ score.sql
@@ -0,0 +1,7 @@
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (1, "date", 10, 20, 10, 100, 1);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (2, "date", 10, 20, 10, 200, 1);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (3, "date", 10, 20, 10, 600, 2);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (4, "date", 10, 20, 10, 200, 4);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (5, "date", 10, 20, 10, 400, 5);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (6, "date", 10, 20, 10, 300, 6);
+insert into score (id, date, learning, questions, referrals, total, user_id ) values (7, "date", 10, 20, 10, 500, 3);
